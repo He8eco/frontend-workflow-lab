9a0cff4a54c6373f0997d63c79e992126b826203
He8eco <He8eco@yandex.ru>
2026-05-12 22:13:17 +0300

    feat: load games with use effect

--- react-fundamentals/src/api/gameApi.js
+++ react-fundamentals/src/api/gameApi.js
@@ -1,5 +1,13 @@
 import { games } from '../data/games'
 
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
 export async function getGames() {
+  await sleep(500)
+
   return games
 }
